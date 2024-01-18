83f73d50b7c147dd9cfd627d095db88dd6da1973
chenhadad2 <chen.hadad@post.runi.ac.il>
2024-01-18 14:26:07 +0200

    14:26

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,25 +1,92 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+      int [] array = {0,1,3};
+      System.out.println(findMissingInt(array));
     }
     
+   
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int sum = 0;
+        for (int i = 0; i < array.length+1; i++){
+            sum = sum + i;
+        }
+        int sumArray = 0;
+        for (int j = 0; j < array.length; j++ ) {
+            sumArray = sumArray + array[j];
+        }
+        return sum-sumArray;
     }
+       
+
+    
+        
+    
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = -999;
+        for (int i = 0; i< array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+        }
+        int secondMaxValue = -999;
+        for (int j = 0; j < array.length; j++) {
+            if ((array[j] != max) && (array[j] > secondMaxValue)) {
+                secondMaxValue = array[j];
+            }
+        }
+        return secondMaxValue;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
+        for (int i = 0; i < array1.length; i++) { 
+            if (isInArray(array2, array1[i]) == false){
+                return false;
+            }
+        }
+        for (int i = 0; i < array2.length; i++) { 
+            if (isInArray(array1, array2[i]) == false){
+                return false;
+            }
+        }
+
         // Write your code here:
-        return false;
+        return true;
+    }
+
+    public static boolean isInArray(int [] array, int number){
+        boolean isInArray = false;
+        for (int j = 0; j < array.length; j++) {
+           if (number== array[j]) {
+                isInArray = true;
+           }
+        }
+        return isInArray;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
+        boolean up = true;
+        
+        for (int i = 0; i < array.length -1; i++) {
+            if (array[i] > array[i+1]) {
+                up = false;
+            }
+
+        }
+        if(up == true){
+            return true;
+        }
+        boolean down = true;
+        for (int i = 0; i < array.length -1; i++) {
+            if (array[i] < array[i+1]) {
+                down = false;
+            }
+
+        }
+        if(down == true){
+            return true;
+        }
         return false;
     }
 
--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -22,21 +24,102 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String str = "hfaih";
+        int [] array = {0,1,3};
+        System.out.println(camelCase(str));
+       
+       
+        System.out.println(Arrays.toString(allIndexOf("hello", 'h')));   
         
+        
+    }
+    public static String lowerCase(String s) { // function that returns the lower case version of the input string
+        String newStr = ""; // init a new str
+        for(int i=0; i<s.length();i++) { // runs on all chars of the string
+            if ((s.charAt(i) >= 65) && (s.charAt(i) <= 90)) { // checks if char is UPPERCASE letter
+                int temp = s.charAt(i) + 32; // checks lowercase ASCII code
+                newStr += (char) temp; // writes lowercase letter instead
+            } else newStr += s.charAt(i);
+        }
+        return newStr;
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+        String Str = lowerCase(string);
+       String newStr = "";
+
+        for (int i = 0; i < string.length(); i++ ) {
+            char newChar = Str.charAt(i);
+            if ((Str.charAt(i) == 'e') || (Str.charAt(i) == 'a') || (Str.charAt(i) == 'i') || (Str.charAt(i) == 'u') || (Str.charAt(i) == 'o') ) {
+                newChar = (char) (Str.charAt(i) - 32);
+                
+            }
+              
+                        newStr = newStr+ newChar;
+            }
+        
+        return newStr;
+        }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String str = "";
+        String low = "";
+
+      for (int i = 0; i < string.length(); i++) {
+        char var2 = string.charAt(i);
+        if (string.charAt(i)>= 'A' && string.charAt(i) <= 'Z') {
+            var2 = (char) (string.charAt(i) + 32);
+        }
+        low = low +var2;
+        
+        }
+        for (int j = 0; j < low.length(); j++) {
+            char mid = low.charAt(j);
+            if (mid == 32) {
+                while (mid == 32) {
+                    if (j == low.length() - 1) {
+                        break;
+                    }
+                    mid = low.charAt(j + 1);
+                    j++;
+                }
+                if (low.charAt(0) != 32 || str != "") 
+                mid = (char) (low.charAt(j) - 32);
+            }
+            str = str + mid;
+
+        }
+
+      
+      
+        return str;
     }
+    
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char var2 = string.charAt(i);
+            if (var2 == chr) {
+                count++;       
+        }
     }
+        int [] index = new int[count];
+        int x = 0;
+        for (int j = 0; j < string.length(); j++ ) {
+            char var3 = string.charAt(j);
+            if (var3 == chr) {
+                index [x] = j;
+                x++;
+              
+            }
+            }
+            
+    
+    return index;
 }
+}
+
+
+
